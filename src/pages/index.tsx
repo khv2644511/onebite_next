b159fc076be610a9584a8fc804b64f71d1f2c17c
khv2644511 <khv2644511@naver.com>
2025-04-01 11:07:06 +0900

    test: how to work in SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,35 @@
 // CSS Module
 import SearchableLayout from '@/components/searchable-layout';
 import style from './index.module.css';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 import books from '@/mock/books.json';
 import BookItem from '@/components/book-item';
+import { InferGetServerSidePropsType } from 'next';
 
-export default function Home() {
+export const getServerSideProps = () => {
+  // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터 불러오는 함수
+
+  // window.location; // ReferenceError: window is not defined
+  console.log('서버사이드 프롭스에요');
+
+  const data = 'hello';
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+export default function Home({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log('data', data);
+  // window.location; // ReferenceError: window is not defined
+
+  useEffect(() => {
+    // ✅ 서버에서 실행되지 않고, 오직 브라우저에서만 컴포넌트가 마운트될 때 실행되기 때문에 window 사용 가능
+    console.log(window);
+  });
+
   return (
     <div className={style.container}>
       <section>
