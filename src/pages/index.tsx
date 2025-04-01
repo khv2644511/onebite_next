f274173b048fb336673ebfc799f9abfd1d04b574
khv2644511 <khv2644511@naver.com>
2025-04-01 13:18:41 +0900

    FEAT: fetch allBooks&randomBooks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,42 +5,37 @@ import { ReactNode, useEffect } from 'react';
 import books from '@/mock/books.json';
 import BookItem from '@/components/book-item';
 import { InferGetServerSidePropsType } from 'next';
+import fetchBooks from '@/lib/fetch-books';
+import fetchRandomBooks from '@/lib/fetch-random-books';
 
-export const getServerSideProps = () => {
-  // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터 불러오는 함수
-
-  // window.location; // ReferenceError: window is not defined
-  console.log('서버사이드 프롭스에요');
-
-  const data = 'hello';
+export const getServerSideProps = async () => {
+  const [allBooks, recoBooks] = await Promise.all([
+    // 병렬로 함수 호출하기
+    fetchBooks(), //
+    fetchRandomBooks(),
+  ]);
 
   return {
     props: {
-      data,
+      allBooks,
+      recoBooks,
     },
   };
 };
 
-export default function Home({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  console.log('data', data);
-  // window.location; // ReferenceError: window is not defined
-
-  useEffect(() => {
-    // ✅ 서버에서 실행되지 않고, 오직 브라우저에서만 컴포넌트가 마운트될 때 실행되기 때문에 window 사용 가능
-    console.log(window);
-  });
-
+export default function Home({ allBooks, recoBooks }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(recoBooks);
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
+        {recoBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
